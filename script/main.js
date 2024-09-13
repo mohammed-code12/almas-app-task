20974d345c710c6871e7a823bad6c366c7ddd4c6
mohammed-code12 <mdrazamdsaeed@gmail.com>
2024-09-13 11:05:56 +0530

    almas-app-done

---
+++ script/main.js
@@ -0,0 +1,27 @@
+var index = 0;
+var slides = document.querySelectorAll(".slide");
+
+function changeSlide(){
+
+  if(index<0){
+    index = slides.length-1;
+  }
+  
+  if(index>slides.length-1){
+    index = 0;
+  }
+  
+  for(let i=0;i<slides.length;i++){
+    slides[i].style.display = "none";
+  }
+  
+  slides[index].style.display= "block";
+  
+  index++;
+  
+  setTimeout(changeSlide,2000);
+  
+}
+
+changeSlide();
+
